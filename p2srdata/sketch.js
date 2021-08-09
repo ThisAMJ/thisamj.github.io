a43fec1a516fcdd5eadf00ec1d2ddc39801aa325
Jesse <69196954+ThisAMJ@users.noreply.github.com>
2021-08-09 16:18:10 +1000

    minor formatting
    link to category pages
    'not by default' for cm nativity if has mtriggers

--- p2srdata/sketch.js
+++ p2srdata/sketch.js
@@ -4,10 +4,10 @@ function display() {
 	let map = data.maps[document.querySelector('#slider').value];
 		let desired = [];
 		desired.push(map.formattedWiki);
-		desired.push('Categories: ' + map.categories.join(', '));
+		desired.push('Categories: ' + map.categories.map(e => `<a href="https://wiki.portal2.sr/Category:${e}" target="_blank">${e}</a>`).join(', '));
 		desired.push('Mtriggers: ' + (map.triggers.length > 0 ? `<br><pre>${map.triggers.join('\n')}</pre>` : 'None'));
 		desired.push('Fade: ' + (map.fade == '' ? 'None' : map.fade));
-		desired.push('Native to CM: ' + (map.cmNative ? 'Yes' : 'No'));
+		desired.push('Native to CM: ' + (map.cmNative ? 'Yes' : map.triggers.length > 0 ? 'Not by default' : 'No'));
 		desired.push(`<a href="https://wiki.portal2.sr/index.php?title=${map.wikiname}&action=edit" target="_blank">Edit this page</a><br>
 					  <a href="https://wiki.portal2.sr/index.php?title=${map.wikiname}&action=history" target="_blank">View history of this page</a>`);
 		document.querySelector('pre').innerHTML = desired.join('<br><br>').replaceEvery('<br><br><br>', '<br><br>').replaceEvery('<br><pre>', '<pre>').replaceEvery('</pre><br>', '</pre>');
