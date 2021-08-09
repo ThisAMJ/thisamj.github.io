031113563bb2f29afa2eb909d7e2311a5f32d204
Jesse <69196954+ThisAMJ@users.noreply.github.com>
2021-08-10 09:23:15 +1000

    add link to chamber board

--- p2srdata/sketch.js
+++ p2srdata/sketch.js
@@ -8,6 +8,7 @@ function display() {
 		desired.push('Mtriggers: ' + (map.triggers.length > 0 ? `<br><pre>${map.triggers.join('\n')}</pre>` : 'None'));
 		desired.push('Fade: ' + (map.fade == '' ? 'None' : map.fade));
 		desired.push('Native to CM: ' + (map.cmNative ? 'Yes' : map.triggers.length > 0 ? 'Not by default' : 'No'));
+		desired.push(map.chamberID > -1 ? `<a href="https://board.portal2.sr/chamber/${map.chamberID}" target="_blank">View CM Leaderboard</a>` : '');
 		desired.push(`<a href="https://wiki.portal2.sr/index.php?title=${map.wikiname}&action=edit" target="_blank">Edit this page</a><br>
 					  <a href="https://wiki.portal2.sr/index.php?title=${map.wikiname}&action=history" target="_blank">View history of this page</a>`);
 		document.querySelector('pre').innerHTML = desired.join('<br><br>').replaceEvery('<br><br><br>', '<br><br>').replaceEvery('<br><pre>', '<pre>').replaceEvery('</pre><br>', '</pre>');
