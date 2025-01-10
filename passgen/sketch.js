a7c55360294138cb422b7b33b33b9e5f02f618e0
AMJ <69196954+ThisAMJ@users.noreply.github.com>
2025-01-10 19:30:40 +1100

    feat: crypto passgen

--- passgen/sketch.js
+++ passgen/sketch.js
@@ -1,15 +1,26 @@
+function unique(arr) {
+    return [...new Set(arr)].join('');
+}
 
 alphabets = {
     alphalower: "abcdefghijklmnopqrstuvwxyz",
     alphaupper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    alpha: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numeric: "1234567890",
+    alphanumeric: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
     asciisymbols: "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
 };
+alphabets.all = unique(Object.keys(alphabets).map(key => alphabets[key]).join(''));
 
-alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()`~-_=+[{]}\\|;:'\",<.>/?";
-pass = '';
-for (let i = 0; i < 32; i++) {
-    // crypto.getRandomValues? crypto.subtle.generateKey?
-    pass += alphabet[parseInt(Math.random() * alphabet.length)];
+function generate(length, alphabet) {
+    let pass = '';
+    for (let i = 0; i < length; i++) {
+        // naive version:
+        // pass += alphabet[parseInt(Math.random() * alphabet.length)];
+        // cryptographically secure rng:
+        pass += alphabet[parseInt(crypto.getRandomValues(new Uint32Array(1))[0] / 0xFFFFFFFF * alphabet.length)];
+    }
+    return pass;
 }
-console.log(pass);
+
+console.log(generate(32, alphabets.all));
